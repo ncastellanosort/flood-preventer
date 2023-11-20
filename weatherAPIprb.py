22296eb0a5e2a4672fd14f91a8f742b9306c0e13
Nicolas Castellanos <nicolasfelipe00234@gmail.com>
2023-11-19 23:17:06 -0500

    cambiando el departamento de Arauca a Cordoba

--- weatherAPIprb.py
+++ weatherAPIprb.py
@@ -1,6 +1,6 @@
 import urequests
 
-uri = 'https://api.openweathermap.org/data/2.5/weather?q=Arauca&appid=84c3b570d602a28aa69e9796925026a1&units=metric&lang=es'
+uri = 'https://api.openweathermap.org/data/2.5/weather?q=Cordoba,CO&appid=84c3b570d602a28aa69e9796925026a1&units=metric&lang=es'
 
 respuesta = urequests.get(uri)
 
